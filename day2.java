dc76fcebbcacda72c72a0166f27c381645594931
Padmashree05 <90086779+Padmashree05@users.noreply.github.com>
2021-09-04 13:29:57 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,15 @@
+//day 2
+package secondjavaclass;
+
+
+public class day2 {
+    public static void main(String args[]) {
+        int a,b,c;
+     
+        a = Integer.parseInt(args[0]);
+        b = Integer.parseInt(args[1]);
+        c = a+b;
+        System.out.println("Sum = " + c);
+      
+    }
+}
